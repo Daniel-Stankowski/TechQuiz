1c6cf20d15fb5c1712ca40e2b82961a554084901
Daniel Stańkowski <daniostan909v2@gmail.com>
2022-12-07 20:41:04 +0100

    Backend init

---
+++ backend/src/test/java/pl/techquiz/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.techquiz.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
