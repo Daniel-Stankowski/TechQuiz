5fa836ee3caeaf231626e3e5922ad27cc1ce58e6
MarvelousMarcin <01159356@pw.edu.pl>
2022-12-08 16:33:18 +0100

    refactor

--- frontend/src/CategoriesList.js
+++ frontend/src/CategoriesList.js
@@ -1,10 +1,30 @@
-import Category from "./Category"
+import Category from "./Category";
 
-const categoriesData = [['Linux', 99], ['BASH', 77], ['PHP', 181], ['Docker', 125], ['HTML', 139], ['MySQL', 153], ['WordPress', 66], ['Laravel', 10], ['Kubernetes', 136], ['JavaScript', 25], ['DevOps', 23]]
+const categoriesData = [
+  ["Linux", 99],
+  ["BASH", 77],
+  ["PHP", 181],
+  ["Docker", 125],
+  ["HTML", 139],
+  ["MySQL", 153],
+  ["WordPress", 66],
+  ["Laravel", 10],
+  ["Kubernetes", 136],
+  ["JavaScript", 25],
+  ["DevOps", 23],
+];
 
 function CategoriesList() {
-    let categories = categoriesData.map( (category) => <Category name={category[0]} count={category[1]}></Category>)
-    return <div className="center-h"><div className="d-flex flex-row flex-shrink-0 flex-wrap center justify-content-center">{categories}</div></div>
+  let categories = categoriesData.map((category) => (
+    <Category name={category[0]} count={category[1]}></Category>
+  ));
+  return (
+    <div className="center-h">
+      <div className="d-flex flex-row flex-shrink-0 flex-wrap center justify-content-center">
+        {categories}
+      </div>
+    </div>
+  );
 }
 
-export default CategoriesList
+export default CategoriesList;
--- frontend/src/Category.js
+++ frontend/src/Category.js
@@ -1,26 +1,31 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import { useNavigate } from "react-router-dom";
 
 function Category(props) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-            <div className='p-4' style={{cursor: 'pointer'}} onClick={(e) => {
-                navigate("/quiz/" + props.name.toLowerCase());
-              }}>
-                <Card style={{ width: '12rem' }} bg='light'>
-                    <Card.Img variant="top" src={"assets/" + props.name + ".png"} className="mx-auto d-block mt-3" style={{ width: '96px', height: '96px' }} />
-                    <Card.Body>
-                        <Card.Title>
-                            {props.name}
-                        </Card.Title>
-                        <Card.Text>
-                            Liczba pytań: {props.count}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-            </div>
-    )
+  return (
+    <div
+      className="p-4"
+      style={{ cursor: "pointer" }}
+      onClick={(e) => {
+        navigate("/quiz/" + props.name.toLowerCase());
+      }}
+    >
+      <Card style={{ width: "12rem" }} bg="light">
+        <Card.Img
+          variant="top"
+          src={"assets/" + props.name + ".png"}
+          className="mx-auto d-block mt-3"
+          style={{ width: "96px", height: "96px" }}
+        />
+        <Card.Body>
+          <Card.Title>{props.name}</Card.Title>
+          <Card.Text>Liczba pytań: {props.count}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Category;
